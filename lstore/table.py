b45160feaa7a81325fba1d2dcf5bbc154f93a7c7
Brian Li <bryli@ucdavis.edu>
2026-01-30 16:32:20 -0800

    table.py pseudocode

--- lstore/table.py
+++ lstore/table.py
@@ -6,6 +6,8 @@ RID_COLUMN = 1
 TIMESTAMP_COLUMN = 2
 SCHEMA_ENCODING_COLUMN = 3
 
+# variable for size of page range that can be changed for optimization
+
 
 class Record:
 
@@ -28,9 +30,24 @@ class Table:
         self.page_directory = {}
         self.index = Index(self)
         self.merge_threshold_pages = 50  # The threshold to trigger a merge
+        # variable for list of page ranges
+        # create first page range
         pass
 
     def __merge(self):
         print("merge is happening")
         pass
+
+    # class Table should have functions for adding to PageRange, reading PageRange, etc.
  
+ # class PageRange (each holds base pages for specific range of rows and their corresponding tail pages)
+ 
+    # init function (accepts number of columns and range of records it holds)
+    # set range variable
+    # create 2d array for base pages, init with one base page per column
+    # create 2d array for tail pages, init empty
+    # num records tracker
+
+    # has_capacity function
+
+
